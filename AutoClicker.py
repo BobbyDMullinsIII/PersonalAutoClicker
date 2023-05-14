8261280d2ed6f00a40ceb3d8abc9166045fe4537
BobbyDMullinsIII <bobbymullins20000@gmail.com>
2023-05-14 04:36:00 -0400

    Heavily modified comments and command-line argument code to be much more descriptive.

--- AutoClicker.py
+++ AutoClicker.py
@@ -1,106 +1,145 @@
-# Code taken from GeeksForGeeks https://www.geeksforgeeks.org/how-to-make-a-python-auto-clicker/
-# Modified to my specific use case with command-line arguments
+# Basic original code taken from GeeksForGeeks https://www.geeksforgeeks.org/how-to-make-a-python-auto-clicker/
+# Heavily modified to my specific use case with optional command-line arguments
 
 import sys
 import time
 import threading
-from pynput.mouse import Button, Controller
-  
-# pynput.keyboard is used to watch events of 
-# keyboard for start and stop of auto-clicker
-from pynput.keyboard import Listener, KeyCode
-  
-if len(sys.argv) != 1 and len(sys.argv) != 6:
-        print ("Must have none or 5 arguments: [seconds delay] [mouse button] [start key] [stop key] [exit key]")
-        print (len(sys.argv))
+from pynput.mouse import Button, Controller     # Needed for clicking buttons (left, right, or middle) on mouse
+from pynput.keyboard import Listener, KeyCode   # Needed for start, stop, and exit keys from keyboard
+
+
+# Stores number of arguments into one parameter so len() doesnt have to be called numerous times
+argslength = len(sys.argv)
+
+# Will give too many arguments message if too many command-line arguments are given
+if argslength > 6:
+        print ("Maximum of 5 valid command-line arguments: [seconds_delay] [mouse_button] [start_key] [stop_key] [exit_key]")
+        input ("Number of command-line arguments provided: " + str(argslength - 1))
         exit()
       
-#standard parameters used in program without command-line arguments  
-if len(sys.argv) == 1:
-    delay = 0.001
-    button = Button.left
-    start_key = KeyCode(char='-')
-    stop_key = KeyCode(char='=')
-    exit_key = KeyCode(char='\\')
+# Standard parameters used in program without command-line arguments  
+delay = 0.001
+button = Button.left
+start_key = KeyCode(char='-')
+stop_key = KeyCode(char='=')
+exit_key = KeyCode(char='\\')
     
-#use command-line arguments for parameters if given
-elif len(sys.argv) == 6:
-    delay = float(sys.argv[1])
+# Use command-line arguments for parameters if given
+# Uses various error-checks for each parameter
+if argslength >= 2:
+    # Will give error message if input delay is not a valid number
+    try:
+        delay = float(sys.argv[1])
+    except:
+        print ("Input error: First parameter [seconds_delay] must be a valid number.")
+        input ("Invalid parameter given: '" + sys.argv[1] + "'")
+        exit()
     
-    if sys.argv[2].lower() == "left":
-        button = Button.left      
-    elif sys.argv[2].lower() == "right":
-        button = Button.right
-    elif sys.argv[2].lower() == "middle":
-        button = Button.middle
-           
-    start_key = KeyCode(char=sys.argv[3].lower())
-    stop_key = KeyCode(char=sys.argv[4].lower())
-    exit_key = KeyCode(char=sys.argv[5].lower())
+    if argslength >= 3:   
+        inputmousebutton = sys.argv[2].lower()
+        if inputmousebutton == "left":
+                button = Button.left      
+        elif inputmousebutton == "right":
+                button = Button.right
+        elif inputmousebutton == "middle":
+                button = Button.middle
+            
+        # Will give error message if input mouse button is not 'left', 'right', or 'middle'
+        if inputmousebutton != "left" and inputmousebutton != "right" and inputmousebutton != "middle":
+            print ("Input error: Second parameter [mouse_button] parameter must be 'left', 'right', or 'middle'.")
+            input ("Invalid parameter given: '" + sys.argv[2] + "'")
+            exit()
+            
+        if argslength >= 4:  
+            # Will give error message if input start_key button is not a valid keyboard key
+            try: 
+                start_key = KeyCode(char=sys.argv[3].lower())
+            except:
+                print ("Input error: Third parameter [start_key] must be a valid keyboard key.")
+                input ("Invalid parameter given: '" + sys.argv[3] + "'")
+                exit()
+            
+            if argslength >= 5: 
+                # Will give error message if input stop_key button is not a valid keyboard key
+                try:
+                    stop_key = KeyCode(char=sys.argv[4].lower())
+                except:
+                    print ("Input error: Fourth parameter [stop_key] must be a valid keyboard key.")
+                    input ("Invalid parameter given: '" + sys.argv[4] + "'")
+                    exit()
+                
+                if argslength >= 6:  
+                    # Will give error message if input exit_key button is not a valid keyboard key
+                    try:
+                        exit_key = KeyCode(char=sys.argv[5].lower())
+                    except:
+                        print ("Input error: Fifth parameter [exit_key] must be a valid keyboard key.")
+                        input ("Invalid parameter given: '" + sys.argv[5] + "'")
+                        exit()
   
-# threading.Thread is used 
-# to control clicks
+# Threaded class for controlling mouse clicks
 class ClickMouse(threading.Thread):
     
-  # delay and button is passed in class 
-  # to check execution of auto-clicker
+    #Class is defined with input delay and selected mouse button
     def __init__(self, delay, button):
         super(ClickMouse, self).__init__()
         self.delay = delay
         self.button = button
         self.running = False
         self.program_running = True
-  
+
+    # Method for starting auto-clicking
     def start_clicking(self):
         self.running = True
-  
+
+    # Method for stopping auto-clicking
     def stop_clicking(self):
         self.running = False
-  
+
+    # Method for exiting program auto-clicking
     def exit(self):
         self.stop_clicking()
         self.program_running = False
-  
-    # method to check and run loop until 
-    # it is true another loop will check 
-    # if it is set to true or not, 
-    # for mouse click it set to button 
-    # and delay.
+
+    # Method for running loops that execute auto-clicking
     def run(self):
+        # Loop keeps program running until 'exit_key' pressed and causes program_running to be False
         while self.program_running:
+            # Loop will only execute after 'start_key' pressed and causes 'running' parameter to be True
+            # Loop will stop executing if 'stop_key' pressed and causes 'running' parameter to be False
             while self.running:
                 mouse.click(self.button)
                 time.sleep(self.delay)
             time.sleep(0.1)
-  
-  
-# instance of mouse controller is created
+
+
+# Threaded class instance and controller
 mouse = Controller()
 click_thread = ClickMouse(delay, button)
 click_thread.start()
-  
-  
-# on_press method takes 
-# key as argument
-def on_press(key):
-    
-    # starts auto-clicking if not already auto-clicking
+
+
+# Method for executing code if the start, stop, or exit key is pressed
+def on_keypress(key):
+
+    # Starts auto-clicking if not already auto-clicking and 'start_key' pressed
     if key == start_key:
         if click_thread.running == False:
             click_thread.start_clicking()
-              
-    #stops auto-clicking if already auto-clicking
+    
+    # Stops auto-clicking if already auto-clicking and 'stop_key' pressed
     elif key == stop_key:
         if click_thread.running == True:
             click_thread.stop_clicking()
     
-    #ends script entirely if 'exit_key' pressed
+    # Ends program entirely if 'exit_key' pressed
     elif key == exit_key:  
         if click_thread.running == True:
             click_thread.stop_clicking()  
         click_thread.exit()
         listener.stop()
-  
-# actualy start of executing code
-with Listener(on_press=on_press) as listener:
+
+
+# Listens for start, stop, or exit keys to be pressed
+with Listener(on_press=on_keypress) as listener:
     listener.join()
--- README.md
+++ README.md
@@ -1,2 +1,24 @@
 # PersonalAutoClicker
- Respository for an auto clicker that I made in Python3.
+Repository for an auto clicker that I made in Python3.
+
+Python3 and the pynput library must be installed to run this program.
+
+In order to run, either double click on the 'AutoClicker.py' file, or input one of these in a cmd within the same folder.<br>
+(Not all of these may work, depending on your Python3 installation)
+
+*./AutoClicker.py*
+*python3 AutoClicker.py*
+*py AutoClicker.py*
+
+<br><br>
+Program optional command-line arguments:
+
+**[seconds_delay]** - Number of seconds between each delay. (Must be full or decimal number)
+
+**[mouse_button]** - Mouse button to auto-click. (Must be 'left', 'right', or 'middle')
+
+**[start_key]** - Key to start auto-clicking. (Must be valid keyboard key)
+
+**[stop_key]** - Key to stop auto-clicking. (Must be valid keyboard key)
+
+**[exit_key]** - Key to fully exit auto-clicker program. (Must be valid keyboard key)
